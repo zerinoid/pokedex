792e98a88b3f966d75da05a52209891330d9322c
zerinoid <leo.zerino@gmail.com>
2023-11-21 18:28:43 -0300

    feat: implement component PokemonDetails

---
+++ src/components/PokemonDetails.tsx
@@ -0,0 +1,34 @@
+import { usePokemonDetailQuery } from "../redux/services/pokemon/pokemonApi";
+import { listFormatter } from "../utils/listFormater";
+
+const PokemonDetails = ({ pokemonName }: { pokemonName: string }) => {
+  const { isUninitialized, isLoading, isError, data } = usePokemonDetailQuery({
+    name: pokemonName,
+  });
+
+  if (isLoading || isUninitialized) {
+    return <p>loading, please wait</p>;
+  }
+
+  if (isError) {
+    return <p>something went wrong</p>;
+  }
+
+  return (
+    <article>
+      <h2>{data.name}</h2>
+      <img src={data.sprites.front_default} alt={data.name} />
+      <ul>
+        <li>id: {data.id}</li>
+        <li>height: {data.height}</li>
+        <li>weight: {data.weight}</li>
+        <li>
+          types:
+          {listFormatter.format(data.types.map((item) => item.type.name))}
+        </li>
+      </ul>
+    </article>
+  );
+};
+
+export default PokemonDetails;

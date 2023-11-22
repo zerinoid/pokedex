acf770fbf12ea4aa3212ed5a1d066277871e05af
zerinoid <leo.zerino@gmail.com>
2023-11-21 23:07:57 -0300

    fix: implement ApiProvider HOC at main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import pokemonApi from "./redux/services/pokemon/pokemonApi.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApiProvider api={pokemonApi}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
+);

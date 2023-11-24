d121ea867f340211c67be4d73d6b8444a4627599
zerinoid <leo.zerino@gmail.com>
2023-11-24 18:55:04 -0300

    feat: restored index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import pokemonApi from "./redux/services/pokemon/pokemonApi.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
